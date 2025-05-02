0e9c3a1e74124ebf7b9d69dd5edb2340ab411f34
leodlcg <142692721+leodlcg@users.noreply.github.com>
2025-05-02 14:43:40 -0700

    Add files via upload

---
+++ Livros/Logica de programacao e algoritmos com JS - Edécio Fernando Lepsen/Cap01/cap06/js/ex6_1.js
@@ -0,0 +1,51 @@
+const frm = document.querySelector("form")
+const respNome = document.querySelector("span")
+const respLista = document.querySelector("pre")
+
+const pacientes = []
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const nome = frm.inPaciente.value
+    pacientes.push(nome)
+
+    let lista = "" // Variavel acumuladora
+    for (let i = 0; i < pacientes.length; i++){
+        lista = lista + `${i + 1} - ${pacientes[i]}\n`
+    }
+
+    respLista.innerText = lista
+
+    frm.inPaciente.value = ""
+    frm.inPaciente.focus()
+})
+
+
+frm.btUrgencia.addEventListener("click", () =>{
+    if(!frm.checkValidity()) {
+        alert("Informe o nome do paciente a ser atendido em carater de urgencia")
+        frm.inPaciente.focus()
+        return
+    }
+    const nome = frm.inPaciente.value
+    pacientes.unshift(nome)
+    let lista = ""
+    pacientes.forEach((paciente, i) => (lista += `${i + 1}. ${paciente}\n`))
+    respLista.innerText = lista
+    frm.inPaciente.value = ""
+    frm.inPaciente.focus()
+})
+
+frm.btAtender.addEventListener("click", () => {
+    if(pacientes.length == 0) {
+        alert("Nao ha pacientes na lista de espera")
+        frm.inPaciente.focus()
+        return
+    }
+    const atender = pacientes.shift()
+    respNome.innerText = atender
+    let lista = ""
+    pacientes.forEach((paciente, i) => (lista += `${i + 1}. ${paciente}\n`))
+    respLista.innerText = lista
+})
