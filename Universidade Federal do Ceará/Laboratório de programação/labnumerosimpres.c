5c9bc0f5fa76bebf97386e8a25fd6d8958fa42a5
leodlcg <142692721+leodlcg@users.noreply.github.com>
2025-05-07 18:53:48 -0700

    Add files via upload

---
+++ Universidade Federal do Ceará/Laboratório de programação/labnumerosimpres.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main(){
+    int impar = 1;
+    int n = 10;
+        for(int i = 0; i < n; i++){
+            printf("%d\n", impar);
+            impar += 2;
+        }
+        printf("\n");
+
+        for(int i = 1; i < 20; i+= 2){
+            printf("%d; ", i);
+        }
+
+    while(1){
+        printf("ahhhh");
+    }
+
+    return 0;    
+}
+
---
+++ Universidade Federal do Ceará/Laboratório de programação/labrepeticao.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main (){
+
+for(int x = 0; x <100; x++){
+
+    printf("%d", x);
+
+}
+
+int y = 0;
+
+while (y < 100){
+    printf("%d", y); 
+    y++;   /* code */
+}
+    return 0;
+}
