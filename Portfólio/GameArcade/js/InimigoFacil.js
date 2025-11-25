e1a0d4f9b8c02219af706f1a188dae59c23b68d4
leodlcg <142692721+leodlcg@users.noreply.github.com>
2025-11-25 19:43:37 -0300

    Add files via upload

---
+++ Portfólio/GameArcade/js/InimigoFacil.js
@@ -0,0 +1,5 @@
+class InimigoFacil extends Inimigo{
+
+constructor(vida, dano, ArrayAtaqueTecla, velocidadeMovimento, ArrayvelocidadeAtaque){
+    super(vida, dano, ArrayAtaqueTecla, velocidadeMovimento, ArrayvelocidadeAtaque);
+}}
