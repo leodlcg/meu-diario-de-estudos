00a59ad85dbbdf33bee292f506897e6e80d78cc9
leodlcg <142692721+leodlcg@users.noreply.github.com>
2025-12-07 12:51:59 -0300

    Delete Portfólio/GameArcade directory

--- Portfólio/GameArcade/js/InimigoFacil.js
+++
@@ -1,5 +0,0 @@
-class InimigoFacil extends Inimigo{
-
-constructor(vida, dano, ArrayAtaqueTecla, velocidadeMovimento, ArrayvelocidadeAtaque){
-    super(vida, dano, ArrayAtaqueTecla, velocidadeMovimento, ArrayvelocidadeAtaque);
-}}
